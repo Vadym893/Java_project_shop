a94caa404d1b8eefe0fc73d3b86e40234286b217
Vadym Pasechnik <makcvadim123@gmail.com>
2025-05-21 10:18:14 +0200

    class of 14.05

---
+++ ecommerce/ecommerce/src/main/java/pl/pasechnik/ecommerce/sales/AcceptOfferCommand.java
@@ -0,0 +1,10 @@
+package pl.pasechnik.ecommerce.sales;
+
+public class AcceptOfferCommand {
+    String fname;
+    String lname;
+    String email;
+    public void makeReservationPaid(String reservationId){
+        
+    }
+}
---
+++ ecommerce/ecommerce/src/main/java/pl/pasechnik/ecommerce/sales/SalesFacade.java
@@ -0,0 +1,10 @@
+package pl.pasechnik.ecommerce.sales;
+
+public class SalesFacade {
+    public void addToCart(String productId){
+
+    }
+    public void AcceptOrder(AcceptOfferCommand AcceptOffer){
+
+    }
+}
